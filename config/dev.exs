a702ea8a18bc328475d008b85daa9075de687180
freddneos <fredd@neosdev.com.br>
2020-12-19 01:24:56 +0000

    configs

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :trello_tasker, TrelloTasker.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "admin",
+  password: "admin",
   database: "trello_tasker_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
